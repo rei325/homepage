5d1be488a4546a5e05af1050d673b3a9bfa8c06d
久志本 <k224t056@st.kyoto-seika.ac.jp>
2026-07-16 16:43:06 +0900

    aaa

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = resolve(fileURLToPath(import.meta.url), '..');
+
+export default defineConfig({
+  base: '/homepage/',
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
+      },
+    },
+  },
+});
